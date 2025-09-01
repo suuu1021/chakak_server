8615fb88d061def36c6e1f0fa722e05a9290ba3b
jang seungwon <rhanwnf97@naver.com>
2025-09-01 15:38:26 +0900

    feat:유저프로필등록, define추가

---
+++ src/main/java/com/green/chakak/chakak/account/user_profile/UserProfileRequest.java
@@ -0,0 +1,40 @@
+package com.green.chakak.chakak.account.user_profile;
+
+import com.green.chakak.chakak.account.user.LoginUser;
+import com.green.chakak.chakak.account.user.User;
+import jakarta.validation.constraints.NotEmpty;
+import lombok.Data;
+
+public class UserProfileRequest {
+
+    @Data
+    public static class CreateDTO {
+        @NotEmpty(message = "닉네임을 입력하세요")
+        private String nickName;
+        @NotEmpty(message = "자기소개를 입력하세요")
+        private String introduce;
+
+        public UserProfile toEntity(User user){
+            return UserProfile.builder()
+                    .user(user)
+                    .nickName(nickName)
+                    .introduce(introduce)
+                    .build();
+        }
+    }
+
+    public static class UpdateDTO {
+        @NotEmpty(message = "닉네임을 입력하세요")
+        private String nickName;
+        @NotEmpty(message = "자기소개를 입력하세요")
+        private String introduce;
+
+        public UserProfile toEntity(LoginUser loginUser , User user){
+            return UserProfile.builder()
+                    .user(user)
+                    .nickName(nickName)
+                    .introduce(introduce)
+                    .build();
+        }
+    }
+}
